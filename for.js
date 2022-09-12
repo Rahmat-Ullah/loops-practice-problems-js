e5c4ed3de07bc6e3b1122a2df5f699b3054a34c0
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-12 19:23:09 +0600

    for loop practiced

---
+++ for.js
@@ -0,0 +1,24 @@
+// var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log('Roast Den na , Painai to!!!');
+//     console.log(roastGiven);
+//     roastGiven++;
+// }
+
+// for(var roastGiven=0; roastGiven < 7; roastGiven++){
+//     console.log(roastGiven);
+//     console.log('roast den na, pai nai to!!!!!!');
+// }
+
+//simple version of for loop
+
+// for(var i=0; i<7; i++){
+//     console.log(i);
+// }
+
+for(var i = 0; i<=20; i+=2){
+    console.log(i);
+}
+for(var i = 1; i<=20; i+=2){
+    console.log(i);
+}
