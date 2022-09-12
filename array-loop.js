419b1206542cbe9cf8a9ca3749ea9deaeb0d35ec
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-12 19:40:30 +0600

    array in loop of js practiced

---
+++ array-loop.js
@@ -0,0 +1,18 @@
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+// display all element of an array
+var numbers = [12, 34, 45, 56, 67, 78, 89];
+
+for(var i = 0; i < 7; i++){
+    var number = numbers[i];
+    console.log(number);
+}
+
+var items = ['laptop', 'soundbox', 'monitor', 'mouse'];
+
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
