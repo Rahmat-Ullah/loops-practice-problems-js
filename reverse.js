e9bdd34d486976df5ecbe5644dcf02bf992a9484
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-12 20:23:03 +0600

    some changes added

--- reverse.js
+++ reverse.js
@@ -13,3 +13,14 @@ while(num >= 1){
 for(i = 10; i>=1; i--){
     console.log(i);
 }
+var name = "false";
+console.log(typeof name);
+
+var i = 5;
+for (; i < 5; i++) {
+  console.log(i);
+}
+
+var i = 0;
+for (i=0; i<5; i++){};
+console.log(i);
